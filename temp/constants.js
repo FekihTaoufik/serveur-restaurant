76f1aacd073e19e97b31e577bf7249273d251b22
Taoufik <tafekih@outlook.fr>
2020-01-09 21:30:19 +0100

    creation du 1er dataset de test

---
+++ temp/constants.js
@@ -0,0 +1,35 @@
+module.exports = {
+  CUISINE_TYPES: [
+    "moléculaire",
+    "gastronomique",
+    "exotique",
+    "nouvelle cuisine",
+    "traditionnelle"
+  ],
+  PLAT_TYPES: [
+    "Accompagnements‎",
+    "Amuse bouches‎",
+    "Bases‎",
+    "Boissons‎",
+    "Confitures‎",
+    "Desserts‎",
+    "Entrées‎",
+    "Fondues‎",
+    "Fritures‎",
+    "Galettes‎",
+    "Pains‎",
+    "Patés‎",
+    "Pâtes alimentaires‎",
+    "Petits-déjeuners‎",
+    "Plat principal‎",
+    "Poissons‎",
+    "Recettes de pizzas‎",
+    "Recettes de ragoût‎",
+    "Recettes de tartes‎",
+    "Salades‎",
+    "Sandwiches‎",
+    "Soupes‎",
+    "Sucreries‎",
+    "Viande‎"
+  ]
+};
